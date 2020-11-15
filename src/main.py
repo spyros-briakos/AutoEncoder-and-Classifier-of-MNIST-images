a809f02b550f8bfba06777a7c025c6e035b36712
Spyros Briakos <spympr.6@gmail.com>
2020-11-15 18:20:44 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+import pandas as pd
+import keras
+from keras import layers, optimizers, losses, metrics
+
+# model = keras.Sequential()
+# model.add(layers.Dense(64, activation='relu', input_shape=(10,)))
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(10, activation='softmax'))
