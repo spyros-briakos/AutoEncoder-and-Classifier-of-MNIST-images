081df99524b5e3cb7d2c4d5cbd54ee82ffb3f49e
Alexandra Apostolopoulou <alexaaposto@gmail.com>
2020-11-16 23:05:21 +0200

    update

--- src/main.py
+++ src/main.py
@@ -9,7 +9,11 @@ import matplotlib.pyplot as plt
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
-from kerasimport layers, optimizers, losses, metrics
+# from keras import layers, optimizers, losses, metrics
+# import keras
+from tensorflow.keras.layers import Input,Conv2D,MaxPooling2D,UpSampling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import RMSprop
 
 def main(argv):
     if(len(sys.argv) != 3):
@@ -32,7 +36,6 @@ def main(argv):
         titles_2_show.append('training image [' + str(r) + ']')  
 
     show_images(images_2_show, titles_2_show)
-
     
     np.random.seed(1)
     tf.random.set_seed(1)
@@ -42,14 +45,67 @@ def main(argv):
     intermediate_dim = 64
     original_dim = 784
 
-    X_train = Load_Mnist_Images(train_file)
+    X_train = images 
+
     X_train = X_train / np.max(X_train)
-    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2])
+    # print(X_train.shape[0])
+    # print(X_train.shape[1])
+    # print(X_train.shape[2])
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2])
     X_train = X_train.astype('float32')
 
+    # print(type(X_train))
+    print("Training set (images) shape: {shape}".format(shape=X_train.shape))
+
+    # autoencoder = Model(input_img, decoder(encoder(input_img)))
+    # autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
 
 
-def encoder(input image):
+
+def AutoEncoder(input_image):
+    convolution_layer = Encoder(input_image)
+    decoded_layer = Decoder(convolution_layer)
+    return decoded_layer
+
+def Encoder(input_image):
+    #input = 28 x 28 x 1 (wide and thin)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2) #7
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv3) #7
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    
+    return conv4
+
+def Decoder(conv4):
+    conv5 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    conv6 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv5) #7 x 7 x 64
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv6)
+    conv6 = BatchNormalization()(conv6)
+    up1 = UpSampling2D((2,2))(conv6) #14 x 14 x 64
+    conv7 = Conv2D(32, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 32
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv7)
+    conv7 = BatchNormalization()(conv7)
+    up2 = UpSampling2D((2,2))(conv7) # 28 x 28 x 32
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
+    
+    return decoded
 
 
 def show_images(images, title_texts):
